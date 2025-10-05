d6834994eee1941ee51327cefdd276a2022df617
Noé Plantier <plantiernoe50@gmail.com>
2025-10-05 23:08:53 +0200

    package.json optimized

--- ti_padel/next.config.js
+++ ti_padel/next.config.js
@@ -1,12 +1,18 @@
-// filepath: next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: '',
 }
 
 module.exports = nextConfig
