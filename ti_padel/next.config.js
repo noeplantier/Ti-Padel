81bd240c2c8eeffe2eb856e6b3af091bb1663984
Noé Plantier <plantiernoe50@gmail.com>
2025-10-05 23:44:20 +0200

    netlify.toml optimized

--- ti_padel/next.config.js
+++ ti_padel/next.config.js
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
-  basePath: '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
