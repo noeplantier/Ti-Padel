dd33f5241318596a8a4bcf375bb0fc9e63af7c91
Noé Plantier <plantiernoe50@gmail.com>
2025-10-06 00:15:12 +0200

    Fix: Ajout des fichiers manquants

--- ti_padel/next.config.js
+++ ti_padel/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  images: {
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  }
 }
 
 module.exports = nextConfig
